fb41f5ddea813c0768d90f2601a78d42f652081c
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2014-04-03 22:28:01 +0400

    Dumb client implementation

--- src/main/java/jetbrains/teamcity/rest/client/impl/TeamCityClientImpl.java
+++ src/main/java/jetbrains/teamcity/rest/client/impl/TeamCityClientImpl.java
@@ -2,120 +2,121 @@ package jetbrains.teamcity.rest.client.impl;
 
 import jetbrains.teamcity.rest.client.TeamCityClient;
 import jetbrains.teamcity.rest.client.resources.*;
+import jetbrains.teamcity.rest.client.resources.impl.*;
 import org.jetbrains.annotations.NotNull;
 
 public class TeamCityClientImpl implements TeamCityClient {
     @NotNull
     @Override
     public AgentPoolsResource getAgentPoolsResource() {
-        return null;
+        return new AgentPoolsResourceImpl();
     }
 
     @NotNull
     @Override
     public AgentsResource getAgentsResource() {
-        return null;
+        return new AgentsResourceImpl();
     }
 
     @NotNull
     @Override
     public BuildQueueResource getBuildQueueResource() {
-        return null;
+        return new BuildQueueResourceImpl();
     }
 
     @NotNull
     @Override
     public BuildTypesResource getBuildTypesResource() {
-        return null;
+        return new BuildTypesResourceImpl();
     }
 
     @NotNull
     @Override
     public BuildsResource getBuildsResource() {
-        return null;
+        return new BuildsResourceImpl();
     }
 
     @NotNull
     @Override
     public ChangesResource getChangesResource() {
-        return null;
+        return new ChangesResourceImpl();
     }
 
     @NotNull
     @Override
     public DebugResource getDebugResource() {
-        return null;
+        return new DebugResourceImpl();
     }
 
     @NotNull
     @Override
     public InvestigationsResource getInvestigationsResource() {
-        return null;
+        return new InvestigationsResourceImpl();
     }
 
     @NotNull
     @Override
     public ProblemOccurrencesResource getProblemOccurrencesResource() {
-        return null;
+        return new ProblemOccurrencesResourceImpl();
     }
 
     @NotNull
     @Override
     public ProblemsResource getProblemsResource() {
-        return null;
+        return new ProblemsResourceImpl();
     }
 
     @NotNull
     @Override
     public ProjectsResource getProjectsResource() {
-        return null;
+        return new ProjectsResourceImpl();
     }
 
     @NotNull
     @Override
     public RootResource getResource() {
-        return null;
+        return new RootResourceImpl();
     }
 
     @NotNull
     @Override
     public ServerResource getServerResource() {
-        return null;
+        return new ServerResourceImpl();
     }
 
     @NotNull
     @Override
     public TestOccurrencesResource getTestOccurrencesResource() {
-        return null;
+        return new TestOccurrencesResourceImpl();
     }
 
     @NotNull
     @Override
     public TestsResource getTestsResource() {
-        return null;
+        return new TestsResourceImpl();
     }
 
     @NotNull
     @Override
     public UserGroupsResource getUserGroupsResource() {
-        return null;
+        return new UserGroupsResourceImpl();
     }
 
     @NotNull
     @Override
     public UsersResource getUsersResource() {
-        return null;
+        return new UsersResourceImpl();
     }
 
     @NotNull
     @Override
     public VcsRootInstancesResource getVcsRootInstancesResource() {
-        return null;
+        return new VcsRootInstancesResourceImpl();
     }
 
     @NotNull
     @Override
     public VcsRootsResource getVcsRootsResource() {
-        return null;
+        return new VcsRootsResourceImpl();
     }
 }
