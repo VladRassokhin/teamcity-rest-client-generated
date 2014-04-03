8e7d52d2a978d3e2f5ba82211699430c93409661
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2014-04-03 22:14:44 +0400

    Add client interface

---
+++ src/main/java/jetbrains/teamcity/rest/client/TeamCityClient.java
@@ -0,0 +1,63 @@
+package jetbrains.teamcity.rest.client;
+
+import jetbrains.teamcity.rest.client.resources.*;
+import org.jetbrains.annotations.NotNull;
+
+public interface TeamCityClient {
+    @NotNull
+    AgentPoolsResource getAgentPoolsResource();
+
+    @NotNull
+    AgentsResource getAgentsResource();
+
+    @NotNull
+    BuildQueueResource getBuildQueueResource();
+
+    @NotNull
+    BuildTypesResource getBuildTypesResource();
+
+    @NotNull
+    BuildsResource getBuildsResource();
+
+    @NotNull
+    ChangesResource getChangesResource();
+
+    @NotNull
+    DebugResource getDebugResource();
+
+    @NotNull
+    InvestigationsResource getInvestigationsResource();
+
+    @NotNull
+    ProblemOccurrencesResource getProblemOccurrencesResource();
+
+    @NotNull
+    ProblemsResource getProblemsResource();
+
+    @NotNull
+    ProjectsResource getProjectsResource();
+
+    @NotNull
+    Resource getResource();
+
+    @NotNull
+    ServerResource getServerResource();
+
+    @NotNull
+    TestOccurrencesResource getTestOccurrencesResource();
+
+    @NotNull
+    TestsResource getTestsResource();
+
+    @NotNull
+    UserGroupsResource getUserGroupsResource();
+
+    @NotNull
+    UsersResource getUsersResource();
+
+    @NotNull
+    VcsRootInstancesResource getVcsRootInstancesResource();
+
+    @NotNull
+    VcsRootsResource getVcsRootsResource();
+}
