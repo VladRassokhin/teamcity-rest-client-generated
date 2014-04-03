1b9345b935fd0695d7ad6740278ceb5dba84f771
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2014-04-03 22:15:54 +0400

    Add dumb client implementation

---
+++ src/main/java/jetbrains/teamcity/rest/client/impl/TeamCityClientImpl.java
@@ -0,0 +1,121 @@
+package jetbrains.teamcity.rest.client.impl;
+
+import jetbrains.teamcity.rest.client.TeamCityClient;
+import jetbrains.teamcity.rest.client.resources.*;
+import org.jetbrains.annotations.NotNull;
+
+public class TeamCityClientImpl implements TeamCityClient {
+    @NotNull
+    @Override
+    public AgentPoolsResource getAgentPoolsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public AgentsResource getAgentsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public BuildQueueResource getBuildQueueResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public BuildTypesResource getBuildTypesResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public BuildsResource getBuildsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ChangesResource getChangesResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public DebugResource getDebugResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public InvestigationsResource getInvestigationsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ProblemOccurrencesResource getProblemOccurrencesResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ProblemsResource getProblemsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ProjectsResource getProjectsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public Resource getResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public ServerResource getServerResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public TestOccurrencesResource getTestOccurrencesResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public TestsResource getTestsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public UserGroupsResource getUserGroupsResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public UsersResource getUsersResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public VcsRootInstancesResource getVcsRootInstancesResource() {
+        return null;
+    }
+
+    @NotNull
+    @Override
+    public VcsRootsResource getVcsRootsResource() {
+        return null;
+    }
+}
